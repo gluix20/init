4e3224a1783fbf4d11e407857c1418c06bf9f5d6
Luis Galvez <luis.galvez@ice.com>
2024-07-24 12:58:53 -0400

    Add changes in build folder

--- init.sh
+++ init.sh
@@ -5,14 +5,16 @@ REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CALL_DIR="$(pwd)"
 
 pushd $REPO_DIR > /dev/null
-    git pull origin main > /dev/null
+  git pull origin main > /dev/null
+  if [ "$1" == "load" ]; then
     cp -r load/* $CALL_DIR/.
+  fi
 
-    if [ "$1" == "pod" ]; then
-        cp -r pod/* $CALL_DIR/.
-    fi
+  if [ "$1" == "pod" ]; then
+    cp -r pod/* $CALL_DIR/.
+  fi
 
-    if [ "$1" == "build_dbt" ]; then
-        cp -r build_ice/* $CALL_DIR/.
-    fi
+  if [ "$1" == "build_image" ]; then
+    cp -r build_image/* $CALL_DIR/.
+  fi
 popd
